6af1dcc7dab22b117a24326536feb759f1a97cdb
Kendall Nakai <kendallnakai@gmail.com>
2020-03-29 07:46:27 -0700

    Shapessss

--- src/override/js/functions.js
+++ src/override/js/functions.js
@@ -7,37 +7,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-
-/* scalable functions */ 
-var $div = $("#wrap");
-var divHeight = $div.outerHeight();
-var divWidth = $div.outerWidth();
-
-var $wrapper = $("#scaleable-wrapper");
-
-$wrapper.resizable({
-  resize: doResize
-});
-
-function resize(event, ui) {
-
-  var scale, origin;
-
-  scale = Math.min(
-    ui.size.width / divWidth,
-    ui.size.height / divHeight
-  );
-
-  $div.css({
-    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
-  });
-
-}
-
-var starterData = {
-  size: {
-    width: $wrapper.width(),
-    height: $wrapper.height()
-  }
-}
-resize(null, starterData);

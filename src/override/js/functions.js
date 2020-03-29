ae0d54be7be97f15c90f7f78968e1f503fa260f1
Gideon Tong <gideon@gideontong.com>
2020-03-29 08:52:08 -0700

    Replaced JS to follow Chrome security policy
    It still doesn't work tho

--- src/override/js/functions.js
+++ src/override/js/functions.js
@@ -1,9 +1,19 @@
 /* get login */
 var modal = document.getElementById('idlogin-btn');
+document.getElementById('createAnAccount').addEventListener("click", createAnAccount);
+document.getElementById('loginAccount').addEventListener("click", loginAccount);
 
 /* close login */
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+object.onclick = createAnAccount(){
+    document.getElementById('idlogin-btn').style.display = 'none';
+}
+
+object.onclick = loginAccount(){
+    document.getElementById('idlogin-btn').style.display = 'block';
 }
